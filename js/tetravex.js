0a07b24f34c0b82079c0aa68b3f4131a81e7ce82
Alexander Berson <aberson@yahoo.com>
2021-09-17 11:33:17 +0300

    drag tiles

--- js/tetravex.js
+++ js/tetravex.js
@@ -1,4 +1,4 @@
-let x, y, x0, y0;
+let x, y, x0, y0, rect0;
 
 const showBoard = () => document.querySelector("body").style.opacity = 1;
 
@@ -126,17 +126,31 @@ const wellShuffled = (arr) => {
     return true;
 }
 
-const shuffleTilesEnd = (e) => {
+// const shuffleTilesEnd = (e) => {
 
-    let tile = e.currentTarget;
+//     let tile = e.currentTarget;
 
-    tile.classList.remove("shuffle");
-    tile.removeEventListener('transitionend', shuffleTilesEnd);
+//     tile.classList.remove("shuffle");
+//     tile.removeEventListener('transitionend', shuffleTilesEnd);
+// }
+
+const swapID = (tile1, tile2) => {
+
+    temp = tile1.id;
+    tile1.id = tile2.id;
+    tile2.id = temp;
+
+    // console.log(tile1);
+
+    // console.log(tile2)
+
+
 }
 
 const shuffleTiles = () => {
 
     let tilesOrder = [0,1,2,3,4,5,6,7,8];
+    let temp;
 
     do {
         tilesOrder = shuffle(tilesOrder);
@@ -144,9 +158,11 @@ const shuffleTiles = () => {
 
     let tiles = document.querySelectorAll('.tile');
 
-    for (let tile of tiles) {
-        tile.classList.add("shuffle");
-    }
+    // for (let tile of tiles) {
+    //     tile.classList.add("shuffle");
+    // }
+
+    console.log(tilesOrder);
 
     for (let [i, tile] of tiles.entries()) {
 
@@ -154,14 +170,18 @@ const shuffleTiles = () => {
         let offsetLeft =  destinationTile.offsetLeft - tile.offsetLeft;
         let offsetTop = destinationTile.offsetTop - tile.offsetTop;
 
+        tile.id = tilesOrder.indexOf(i) + 1;
+
+        // tile.classList.add("shuffle");
         tile.style.transform = `translate(${offsetLeft}px, ${offsetTop}px)`;
-        tile.addEventListener('transitionend', shuffleTilesEnd);
+        // tile.addEventListener('transitionend', shuffleTilesEnd);
     }
 }
 
 const startMove = (e) => {
 
     const tile = e.currentTarget;
+    rect0 = tile.getBoundingClientRect();
 
     if (e.type === "touchstart") {
         x = x0 = e.touches[0].clientX;
@@ -203,14 +223,44 @@ const move = (e) => {
     tile.style.transform = `translate(${matrix.m41 + dx}px, ${matrix.m42 + dy}px)`;
 }
 
-const returnEnd = (e) => {
+const win = () => {
+
+    document.querySelectorAll('.tile').forEach((tile, i) => {
+
+        if (tile.id = i) console.log(i);
+
+    });
+}
+
+const swapEnd = (e) => {
 
     let tile = e.currentTarget;
 
     enableTouch();
-    tile.classList.remove("return");
-    tile.classList.remove("move");
-    tile.removeEventListener('transitionend', returnEnd);
+    tile.classList.remove("swap", "move");
+    // tile.classList.remove("move");
+    tile.removeEventListener('transitionend', swapEnd);
+    // win();
+
+}
+
+const getSwapTile = (movingTile) => {
+
+    const tiles = document.querySelectorAll('.tile');
+    const rectTile = movingTile.getBoundingClientRect();
+    const ox = rectTile.left + rectTile.width / 2;
+    const oy = rectTile.top + rectTile.height / 2;
+
+    for (let tile of tiles) {
+
+        if (tile == movingTile) continue;
+
+        const rectTile = tile.getBoundingClientRect();
+
+        if (ox >= rectTile.left && ox <= rectTile.right && oy >= rectTile.top && oy <= rectTile.bottom) {
+            return tile;
+        }
+    }
 }
 
 const endMove = (e) => {
@@ -219,17 +269,35 @@ const endMove = (e) => {
     let style = window.getComputedStyle(tile);
     let matrix = new WebKitCSSMatrix(style.transform);
     let event = new Event('transitionend');
+    let swapTile = getSwapTile(tile);
 
-    tile.classList.add("return");
+    tile.classList.add("swap");
     tile.removeEventListener('touchmove', move);
     tile.removeEventListener('touchend', endMove);
     tile.removeEventListener('mousemove', move);
     tile.removeEventListener('mouseup', endMove);
-    tile.addEventListener('transitionend', returnEnd);
+    tile.addEventListener('transitionend', swapEnd);
 
-    tile.style.transform = `translate(${matrix.m41 - (x - x0)}px, ${ matrix.m42 - (y - y0)}px)`;
+    if (!swapTile) {
+        tile.style.transform = `translate(${matrix.m41 - (x - x0)}px, ${ matrix.m42 - (y - y0)}px)`;
+    } else {
 
-    if (x - x0 == 0 && y - y0 == 0) tile.dispatchEvent(event);
+        let rectSwap = swapTile.getBoundingClientRect();
+        let style = window.getComputedStyle(swapTile);
+        let matrix2 = new WebKitCSSMatrix(style.transform);
+        let offsetLeft =  rect0.left - rectSwap.left;
+        let offsetTop = rect0.top - rectSwap.top;
+
+        swapTile.classList.add("swap");
+        swapTile.addEventListener('transitionend', swapEnd);
+
+        swapID(tile, swapTile);
+
+        tile.style.transform = `translate(${matrix.m41 - (x - x0) - offsetLeft}px, ${ matrix.m42 - (y - y0) - offsetTop}px)`;
+        swapTile.style.transform = `translate(${matrix2.m41 + offsetLeft}px, ${ matrix2.m42 + offsetTop}px)`;
+    }
+
+    if (x == x0 && y == y0) tile.dispatchEvent(event);
 }
 
 const disableTouch = () => {
@@ -248,13 +316,13 @@ const enableTouch = () => {
 
 const init = () => {
 
-    // disableTapZoom();
+    disableTapZoom();
     setBoardSize(3);
-    showBoard();
     headerColors();
     tilesColors();
-    setTimeout(shuffleTiles, 1500);
-    setTimeout(enableTouch, 2500);
+    shuffleTiles();
+    showBoard();
+    setTimeout(enableTouch, 1000);
 }
 
 window.onload = () => {
